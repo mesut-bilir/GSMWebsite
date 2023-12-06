4a8fb93fd1ebab9ad4ace7f75da3fa9fcf8c2588
asus <mesutbilir@outlook.com>
2023-12-06 15:55:30 +0300

    Add project files.

---
+++ Sk/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sk.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Anasayfa()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+        public ActionResult Urunler()
+        {
+            return View();
+        }
+    }
+}
